9ec17c0be9239df30d7b45e2bf135405ca51ba7b
Komal12 <upadhyay.ko@northeastern.edu>
2025-04-30 12:52:34 -0700

    Add loading screen implementation

--- app/src/main/java/com/example/fetchrewardslist/ui/components/ItemScreen.kt
+++ app/src/main/java/com/example/fetchrewardslist/ui/components/ItemScreen.kt
@@ -2,6 +2,7 @@ package com.example.fetchrewardslist.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +11,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -37,7 +40,9 @@ fun ItemScreen(viewModel: FetchListViewModel) {
 
 @Composable
 fun LoadingScreen() {
-
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
+    }
 }
 
 @Composable
