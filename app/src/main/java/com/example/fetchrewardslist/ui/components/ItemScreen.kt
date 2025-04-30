7973ed63e50dc52de0bd033e2c29ff3b5f7e0e26
Komal12 <upadhyay.ko@northeastern.edu>
2025-04-30 13:05:17 -0700

    Add error state ui

--- app/src/main/java/com/example/fetchrewardslist/ui/components/ItemScreen.kt
+++ app/src/main/java/com/example/fetchrewardslist/ui/components/ItemScreen.kt
@@ -1,5 +1,6 @@
 package com.example.fetchrewardslist.ui.components
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -15,11 +16,13 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.fetchrewardslist.model.Item
@@ -52,7 +55,11 @@ fun EmptyScreen() {
 
 @Composable
 fun ErrorScreen(error: String) {
+    val context = LocalContext.current
 
+    LaunchedEffect(error) {
+        Toast.makeText(context, error, Toast.LENGTH_LONG).show()
+    }
 }
 
 @Composable
