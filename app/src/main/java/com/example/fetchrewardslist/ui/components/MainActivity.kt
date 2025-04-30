5ee294d5d50fc0a59947ba8c198d52f02f8e4ac1
Komal12 <upadhyay.ko@northeastern.edu>
2025-04-30 14:10:39 -0700

    Add TopBar changed app icon

--- app/src/main/java/com/example/fetchrewardslist/ui/components/MainActivity.kt
+++ app/src/main/java/com/example/fetchrewardslist/ui/components/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import com.example.fetchrewardslist.remote.RetrofitClient
@@ -14,11 +16,13 @@ import com.example.fetchrewardslist.repository.FetchListRepositoryImpl
 import com.example.fetchrewardslist.ui.theme.FetchRewardsListTheme
 import com.example.fetchrewardslist.viewmodel.FetchListViewModel
 import com.example.fetchrewardslist.viewmodel.FetchListViewModelFactory
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var viewModel: FetchListViewModel
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,7 +33,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FetchRewardsListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(),topBar = {
+                    TopAppBar(
+                        title = { Text("Fetch Rewards Item List") }
+                    )
+                }) { innerPadding ->
                     ItemScreen(viewModel = viewModel, Modifier.padding(innerPadding))
                 }
             }
